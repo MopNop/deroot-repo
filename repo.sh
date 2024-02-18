0f148def759d07f96d34b77be617c37c464d52c2
Echo <98044070+MopNop@users.noreply.github.com>
2024-02-18 20:39:08 +0000

    agdgageagaeghhhs

--- repo.sh
+++ repo.sh
@@ -12,14 +12,14 @@ bzip2 -c9 Packages > Packages.bz2
 
 echo "[Repository] Generating Release..."
 apt-ftparchive \
-		-o APT::FTPArchive::Release::Origin="Sileo" \
-		-o APT::FTPArchive::Release::Label="Sileo" \
+		-o APT::FTPArchive::Release::Origin="Echo's Derooted Repo" \
+		-o APT::FTPArchive::Release::Label="Echo's Derooted Repo" \
 		-o APT::FTPArchive::Release::Suite="stable" \
-		-o APT::FTPArchive::Release::Version="2.0" \
+		-o APT::FTPArchive::Release::Version="1.0" \
 		-o APT::FTPArchive::Release::Codename="ios" \
-		-o APT::FTPArchive::Release::Architectures="iphoneos-arm iphoneos-arm64" \
+		-o APT::FTPArchive::Release::Architectures="iphoneos-arm64" \
 		-o APT::FTPArchive::Release::Components="main" \
-		-o APT::FTPArchive::Release::Description="Sileo for Checkra1n and Unc0ver" \
+		-o APT::FTPArchive::Release::Description="repo with derooted stuff" \
 		release . > Release
 
 
